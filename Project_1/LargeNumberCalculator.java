624fc9bd8dac3109f40596f971bee5d2aa923968
egeoruc <ege.oruc@ozu.edu.tr>
2023-10-05 11:52:10 +0300

    Week 1 my work

---
+++ Project_1/LargeNumberCalculator.java
@@ -0,0 +1,57 @@
+package Project_1;
+
+import Project_1.Node;
+
+public class LargeNumberCalculator {
+
+    private static class ListNode{
+        int digit;
+        Node next;
+
+        public ListNode(int digit){
+            this.digit = digit;
+            this.next = null;
+        }
+    }
+
+    //Function for adding two numbers
+    public String addNumbers(Node num1, Node num2){
+        //Implementation goes here
+        return "";  //Placeholder
+    }
+
+    //Function for subtract two large numbers
+    public String subtractNumbers(Node num1, Node num2){
+        //Implementation goes here
+        return "";  //Placeholder
+    }
+
+    //Function for multiply two large numbers
+    public String multiplyNumbers(Node num1, Node num2){
+        //Implementation goes here
+        return "";  //Placeholder
+    }
+
+    //Function for divide two large numbers
+    public String divideNumbers(Node num1, Node num2){
+        //Implementation goes here
+        return "";  //Placeholder
+    }
+
+    //Helper method to create a linked List to a string 
+    private Node createLinkedList (String number){
+        //Implementation goes here
+        return null;    //Placeholder
+    }
+
+    //helper method to convert linkedList to a string
+    private String convertToString(Node node){
+        //Implementation goes here
+        return "";  //Placeholder
+    }
+
+    public static void main(String[] args){
+        //Test our methods here
+    }
+
+}
---
+++ Project_1/Node.java
@@ -0,0 +1,12 @@
+package Project_1;
+
+public class Node {
+    int digit;
+    Node next;
+
+    public Node(int digit){
+        this.digit = digit;
+        this.next = null;
+
+    }
+}
